29a5f75c04f6ea22a91ff0325fedddf4a87ab4ca
Shadab khan <117011709+imshadabkhan@users.noreply.github.com>
2025-02-22 20:49:26 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/learn_link/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.learn_link
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/constants/asset_constant.dart
@@ -0,0 +1,8 @@
+class ImageConstants {
+  static const basePath = "assets/images";
+  static const main_MathematicImage = "$basePath/main_maths.jpg";
+  static const sub_MathematicImage1 = "$basePath/sub_maths1.jpg";
+  static const sub_MathematicImage2 = "$basePath/sub_maths1.jpg";
+  static const sub_MathematicImage3 = "$basePath/sub_maths1.jpg";
+  static const sub_MathematicImage4 = "$basePath/sub_maths1.jpg";
+}
